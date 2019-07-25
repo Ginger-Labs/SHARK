34134617288e9fe17e05a888945d71d7caf22895
Drew Bratcher <drew@gingerlabs.com>
2019-07-25 13:28:52 -0700

    use builtin tf ops

--- src/convert.py
+++ src/convert.py
@@ -4,7 +4,7 @@ graph_def_file = "../model/output_graph.pb"
 input_arrays = ["Placeholder"]
 output_arrays = ["CTCGreedyDecoder"]
 
-
 converter = tf.lite.TFLiteConverter.from_frozen_graph(graph_def_file, input_arrays, output_arrays)
+converter.target_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
 tflite_model = converter.convert()
 open("converted_model.tflite", "wb").write(tflite_model)
